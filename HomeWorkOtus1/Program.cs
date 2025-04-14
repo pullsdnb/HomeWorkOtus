f67c26a752ff0d99ca9a5367013071276c754296
pullsdnd <pulls2x@gmail.com>
2025-04-14 18:46:59 +0200

    Move the command handlers to separate methods

--- HomeWorkOtus1/Program.cs
+++ HomeWorkOtus1/Program.cs
@@ -194,6 +194,8 @@ public class TaskLengthLimitException : Exception
     public TaskLengthLimitException(int taskLength, int taskLengthLimit)
         : base($"Длина задачи '{taskLength}' превышает максимально допустимое значение {taskLengthLimit}")
     {
+
+
     }
 }
 
